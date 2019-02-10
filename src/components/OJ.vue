ec36475abd7a4c29df5d80e3409a57413bcaae1d
jxtxzzw <jxtxzzw@126.com>
2019-02-10 14:31:43 +0800

    搜集文档示例

---
+++ src/components/OJ.vue
@@ -0,0 +1,151 @@
+<template>
+  <div>
+    <Table :data="tableData1" :columns="tableColumns1" stripe></Table>
+    <div style="margin: 10px;overflow: hidden">
+      <div style="float: right;">
+        <Page :total="100" :current="1" @on-change="changePage"></Page>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+  export default {
+    name: 'OJ',
+    data () {
+      return {
+        tableData1: this.mockTableData1(),
+        tableColumns1: [
+          {
+            title: 'Name',
+            key: 'name'
+          },
+          {
+            title: 'Status',
+            key: 'status',
+            render: (h, params) => {
+              const row = params.row;
+              const color = row.status === 1 ? 'primary' : row.status === 2 ? 'success' : 'error';
+              const text = row.status === 1 ? 'Working' : row.status === 2 ? 'Success' : 'Fail';
+              
+              return h('Tag', {
+                props: {
+                  type: 'dot',
+                  color: color
+                }
+              }, text);
+            }
+          },
+          {
+            title: 'Portrayal',
+            key: 'portrayal',
+            render: (h, params) => {
+              return h('Poptip', {
+                props: {
+                  trigger: 'hover',
+                  title: params.row.portrayal.length + 'portrayals',
+                  placement: 'bottom'
+                }
+              }, [
+                h('Tag', params.row.portrayal.length),
+                h('div', {
+                  slot: 'content'
+                }, [
+                  h('ul', this.tableData1[params.index].portrayal.map(item => {
+                    return h('li', {
+                      style: {
+                        textAlign: 'center',
+                        padding: '4px'
+                      }
+                    }, item)
+                  }))
+                ])
+              ]);
+            }
+          },
+          {
+            title: 'People',
+            key: 'people',
+            render: (h, params) => {
+              return h('Poptip', {
+                props: {
+                  trigger: 'hover',
+                  title: params.row.people.length + 'customers',
+                  placement: 'bottom'
+                }
+              }, [
+                h('Tag', params.row.people.length),
+                h('div', {
+                  slot: 'content'
+                }, [
+                  h('ul', this.tableData1[params.index].people.map(item => {
+                    return h('li', {
+                      style: {
+                        textAlign: 'center',
+                        padding: '4px'
+                      }
+                    }, item.n + '：' + item.c + 'People')
+                  }))
+                ])
+              ]);
+            }
+          },
+          {
+            title: 'Sampling Time',
+            key: 'time',
+            render: (h, params) => {
+              return h('div', 'Almost' + params.row.time + 'days');
+            }
+          },
+          {
+            title: 'Updated Time',
+            key: 'update',
+            render: (h, params) => {
+              return h('div', this.formatDate(this.tableData1[params.index].update));
+            }
+          }
+        ]
+      }
+    },
+    methods: {
+      mockTableData1 () {
+        let data = [];
+        for (let i = 0; i < 10; i++) {
+          data.push({
+            name: 'Business' + Math.floor(Math.random () * 100 + 1),
+            status: Math.floor(Math.random () * 3 + 1),
+            portrayal: ['City', 'People', 'Cost', 'Life', 'Entertainment'],
+            people: [
+              {
+                n: 'People' + Math.floor(Math.random () * 100 + 1),
+                c: Math.floor(Math.random () * 1000000 + 100000)
+              },
+              {
+                n: 'People' + Math.floor(Math.random () * 100 + 1),
+                c: Math.floor(Math.random () * 1000000 + 100000)
+              },
+              {
+                n: 'People' + Math.floor(Math.random () * 100 + 1),
+                c: Math.floor(Math.random () * 1000000 + 100000)
+              }
+            ],
+            time: Math.floor(Math.random () * 7 + 1),
+            update: new Date()
+          })
+        }
+        return data;
+      },
+      formatDate (date) {
+        const y = date.getFullYear();
+        let m = date.getMonth() + 1;
+        m = m < 10 ? '0' + m : m;
+        let d = date.getDate();
+        d = d < 10 ? ('0' + d) : d;
+        return y + '-' + m + '-' + d;
+      },
+      changePage () {
+        // The simulated data is changed directly here, and the actual usage scenario should fetch the data from the server
+        this.tableData1 = this.mockTableData1();
+      }
+    }
+  }
+</script>
--- src/components/ResumeLayout.vue
+++ src/components/ResumeLayout.vue
@@ -6,105 +6,103 @@
     border-radius: 4px;
     overflow: hidden;
   }
-  .layout-header-bar{
-    background: #fff;
-    box-shadow: 0 1px 1px rgba(0,0,0,.1);
-  }
-  .layout-logo-left{
-    width: 90%;
+  .layout-logo{
+    width: 100px;
     height: 30px;
     background: #5b6270;
     border-radius: 3px;
-    margin: 15px auto;
+    float: left;
+    position: relative;
+    top: 15px;
+    left: 20px;
   }
-  .menu-icon{
-    transition: all .3s;
-  }
-  .rotate-icon{
-    transform: rotate(-90deg);
-  }
-  .menu-item span{
-    display: inline-block;
-    overflow: hidden;
-    width: 69px;
-    text-overflow: ellipsis;
-    white-space: nowrap;
-    vertical-align: bottom;
-    transition: width .2s ease .2s;
-  }
-  .menu-item i{
-    transform: translateX(0px);
-    transition: font-size .2s ease, transform .2s ease;
-    vertical-align: middle;
-    font-size: 16px;
-  }
-  .collapsed-menu span{
-    width: 0px;
-    transition: width .2s ease;
-  }
-  .collapsed-menu i{
-    transform: translateX(5px);
-    transition: font-size .2s ease .2s, transform .2s ease .2s;
-    vertical-align: middle;
-    font-size: 22px;
+  .layout-nav{
+    width: 420px;
+    margin: 0 auto;
+    margin-right: 20px;
   }
 </style>
 <template>
   <div class="layout">
     <Layout>
-      <Sider ref="side1" hide-trigger collapsible :collapsed-width="78" v-model="isCollapsed">
-        <Menu active-name="1-2" theme="dark" width="auto" :class="menuitemClasses">
-          <MenuItem name="1-1">
-            <Icon type="ios-navigate"></Icon>
-            <span>Option 1</span>
-          </MenuItem>
-          <MenuItem name="1-2">
-            <Icon type="ios-search"></Icon>
-            <span>Option 2</span>
-          </MenuItem>
-          <MenuItem name="1-3">
-            <Icon type="ios-settings"></Icon>
-            <span>Option 3</span>
-          </MenuItem>
+      <Header>
+        <Menu mode="horizontal" theme="dark" active-name="1" @on-select="changeMenu">
+          <div class="layout-logo"></div>
+          <div class="layout-nav">
+            <MenuItem name="1">
+              <Icon type="ios-navigate"></Icon>
+              Item 1
+            </MenuItem>
+            <MenuItem name="2">
+              <Icon type="ios-keypad"></Icon>
+              Item 2
+            </MenuItem>
+            <MenuItem name="3">
+              <Icon type="ios-analytics"></Icon>
+              Item 3
+            </MenuItem>
+            <MenuItem name="4">
+              <Icon type="ios-paper"></Icon>
+              Item 4
+            </MenuItem>
+          </div>
         </Menu>
-      </Sider>
+      </Header>
       <Layout>
-        <Header :style="{padding: 0}" class="layout-header-bar">
-          <Icon @click.native="collapsedSider" :class="rotateIcon" :style="{margin: '0 20px'}" type="md-menu" size="24"></Icon>
-        </Header>
-        <Content :style="{margin: '20px', background: '#fff', minHeight: '260px'}">
-          Content
-        </Content>
+        <Sider hide-trigger :style="{background: '#fff'}">
+          <Menu active-name="1-2" theme="light" width="auto" :open-names="['1']" @on-select="changeMenu">
+            <Submenu name="1">
+              <template slot="title">
+                <Icon type="ios-navigate"></Icon>
+                Item 1
+              </template>
+              <MenuItem name="1-1">
+                Option 1
+              </MenuItem>
+              <MenuItem name="1-2">Option 2</MenuItem>
+              <MenuItem name="1-3">Option 3</MenuItem>
+            </Submenu>
+            <Submenu name="2">
+              <template slot="title">
+                <Icon type="ios-keypad"></Icon>
+                Item 2
+              </template>
+              <MenuItem name="2-1">Option 1</MenuItem>
+              <MenuItem name="2-2">Option 2</MenuItem>
+            </Submenu>
+            <Submenu name="3">
+              <template slot="title">
+                <Icon type="ios-analytics"></Icon>
+                Item 3
+              </template>
+              <MenuItem name="3-1">Option 1</MenuItem>
+              <MenuItem name="3-2">Option 2</MenuItem>
+            </Submenu>
+          </Menu>
+        </Sider>
+        <Layout :style="{padding: '24px 24px 24px'}">
+          <Content :style="{padding: '24px', minHeight: '680px', background: '#fff'}">
+            <!--<OJTable/>-->
+            <!--<Movie/>-->
+          </Content>
+        </Layout>
       </Layout>
     </Layout>
   </div>
 </template>
 <script>
-  export default {
-    name: 'ResumeLayout',
-    data () {
-      return {
-        isCollapsed: false
-      }
-    },
-    computed: {
-      rotateIcon () {
-        return [
-          'menu-icon',
-          this.isCollapsed ? 'rotate-icon' : ''
-        ];
-      },
-      menuitemClasses () {
-        return [
-          'menu-item',
-          this.isCollapsed ? 'collapsed-menu' : ''
-        ]
-      }
-    },
-    methods: {
-      collapsedSider () {
-        this.$refs.side1.toggleCollapse();
-      }
+import OJTable from './OJ'
+import Movie from './Movie'
+export default {
+  name: 'ResumeLayout',
+  components: {OJTable, Movie},
+  methods: {
+    changeMenu (name) {
+      console.log(name)
     }
+  },
+  mounted () {
+    // this.changeMenu()
   }
+}
 </script>
