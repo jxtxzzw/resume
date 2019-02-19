23e41574c0776acad42bacbf25ba1880d2695266
jxtxzzw <jxtxzzw@126.com>
2019-02-19 20:27:39 +0800

    通过设置background为#FFFFFF来避免动画最后一张切换到第一张时候的不流畅，同时避免设置min-Height

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -5,59 +5,60 @@
     text-align:center;
     color:#fff;
     font-size:20px;
-    /*background:#506b9e;*/
+    background:#FFFFFF;
   }
   .cycle-gallery {
-    position: relative;
-    text-align: center;
-    padding: 40px 200px 33px;
-    margin: 0 0 60px;
-    line-height: 1.6; }
+  position: relative;
+  text-align: center;
+  padding: 40px 200px 33px;
+  margin: 0 0 60px;
+  line-height: 1.6; }
   .cycle-gallery p {
     color : #515a6e;
   }
   .cycle-gallery .img-box {
-    display: block;
-    line-height: 1.5;
-    padding: 0 0 40px; }
+  display: block;
+  line-height: 1.5;
+  padding: 0 0 40px; }
   .cycle-gallery .img-box .img-holder {
-    display: inline-block;
-    vertical-align: middle;
-    margin: 0 22px 0 0;
-    overflow: hidden; }
+  display: inline-block;
+  vertical-align: middle;
+  margin: 0 22px 0 0;
+  overflow: hidden; }
   .cycle-gallery .img-box .img-holder img {
-    width: 100px;
-    height: 100px;
-    border-radius: 50%; }
+  width: 100px;
+  height: 100px;
+  border-radius: 50%; }
   .cycle-gallery .img-box .text-holder {
-    display: inline-block;
-    vertical-align: middle;
-    text-align: left; }
+  display: inline-block;
+  vertical-align: middle;
+  text-align: left; }
   .cycle-gallery .img-box h3 {
-    margin: 0;
-    line-height: 1.33333;
-    color: #635C73; }
+  margin: 0;
+  line-height: 1.33333;
+  color: #635C73;
+  }
   .cycle-gallery .img-box a {
-    font-weight: bold; }
+  font-weight: bold; }
   .cycle-gallery:after {
-    content: "";
-    position: absolute;
-    bottom: 0;
-    left: 180px;
-    right: 180px;
-    height: 1px;
-    background: #f4f4f4;
+  content: "";
+  position: absolute;
+  bottom: 0;
+  left: 180px;
+  right: 180px;
+  height: 1px;
+  background: #f4f4f4;
   }
   .cycle-gallery .date {
-    display: block;
-    font-style: normal;
-    line-height: 1.5;
-    font-weight: normal;
-    color: #848e97; }
+  display: block;
+  font-style: normal;
+  line-height: 1.5;
+  font-weight: normal;
+  color: #848e97; }
 </style>
 <template lang="pug">
   Carousel(autoplay, loop)
-    CarouselItem(v-for="project in projectData",:key="project.id")
+    CarouselItem(v-for="project in projectData", :key="project.id")
       div(class="demo-carousel")
         div(class="cycle-gallery")
           div(class="img-box")
