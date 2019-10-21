7d38f981748b89c81c30dc920896907158c6210b
jxtxzzw <jxtxzzw@126.com>
2019-10-21 21:36:09 +0800

    prerender-spa-plugin

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -10,6 +10,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 const ExtractTextPlugin = require('extract-text-webpack-plugin')
 const OptimizeCSSPlugin = require('optimize-css-assets-webpack-plugin')
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+const PrerenderSpaPlugin = require('prerender-spa-plugin')
 
 const env = process.env.NODE_ENV === 'testing'
   ? require('../config/test.env')
@@ -119,7 +120,14 @@ const webpackConfig = merge(baseWebpackConfig, {
         to: config.build.assetsSubDirectory,
         ignore: ['.*']
       }
-    ])
+    ]),
+
+    new PrerenderSpaPlugin(
+      // 编译后的html需要存放的路径
+      path.join(__dirname, '../dist'),
+      // 列出哪些路由需要预渲染
+      [ '/' ]
+    )
   ]
 })
 
--- config/prod.env.js
+++ config/prod.env.js
@@ -1,4 +1,16 @@
 'use strict'
+
+var path = require('path')
+var PrerenderSpaPlugin = require('prerender-spa-plugin')
+
 module.exports = {
-  NODE_ENV: '"production"'
+  NODE_ENV: '"production"',
+  plugins: [
+    new PrerenderSpaPlugin(
+      // 编译后的html需要存放的路径
+      path.join(__dirname, '../dist'),
+      // 列出哪些路由需要预渲染
+      [ '/' ]
+    )
+  ]
 }
