18a6f8ce2a5d4dd063c9786fb8054b7cdc27dd0d
jxtxzzw <jxtxzzw@126.com>
2019-11-10 19:43:44 +0800

    nuxt.js SSR
    Signed-off-by: jxtxzzw <jxtxzzw@126.com>

---
+++ components/CareerTimeline.vue
@@ -0,0 +1,30 @@
+<template lang="pug">
+  div
+    Timeline(pending)
+      TimelineItem
+        p(class="keyPoint") 微软
+        p(class="title") C+AI Summer Intern 于上海紫竹高新技术开发区
+        p(class="content") 自 2019 年 07 月 01 日至 2019 年 09 月 30 日
+</template>
+
+<script>
+export default {
+  name: 'CareerTimeline'
+}
+</script>
+
+<style scoped>
+  .keyPoint{
+    color: #2d8cf0;
+    font-size: 16px;
+    font-weight: bold;
+  }
+  .title{
+    color: #2db7f5;
+    font-size: 16px;
+    font-weight: bold;
+  }
+  .content{
+    padding-left: 12px;
+  }
+</style>
---
+++ nuxt.config.js
@@ -0,0 +1,62 @@
+
+export default {
+  mode: 'universal',
+  /*
+  ** Headers of the page
+  */
+  head: {
+    title: process.env.npm_package_name || '',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+  /*
+  ** Customize the progress-bar color
+  */
+  loading: { color: '#fff' },
+  /*
+  ** Global CSS
+  */
+  css: [
+    'iview/dist/styles/iview.css'
+  ],
+  /*
+  ** Plugins to load before mounting the App
+  */
+  plugins: [
+    '@/plugins/iview'
+  ],
+  /*
+  ** Nuxt.js dev-modules
+  */
+  buildModules: [
+  ],
+  /*
+  ** Nuxt.js modules
+  */
+  modules: [
+    // Doc: https://axios.nuxtjs.org/usage
+    '@nuxtjs/axios',
+  ],
+  /*
+  ** Axios module configuration
+  ** See https://axios.nuxtjs.org/options
+  */
+  axios: {
+  },
+  /*
+  ** Build configuration
+  */
+  build: {
+    /*
+    ** You can extend webpack config here
+    */
+    extend (config, ctx) {
+    }
+  }
+}
