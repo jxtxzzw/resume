4db378b36b8a6d3c5b0c2a1b268073a4c9c07632
jxtxzzw <jxtxzzw@126.com>
2020-02-29 16:02:44 +0800

    默认使用中文的 iView
    Signed-off-by: jxtxzzw <jxtxzzw@126.com>

--- plugins/iview.js
+++ plugins/iview.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import iView from 'iview'
-import locale from 'iview/dist/locale/en-US' // Change locale, check node_modules/iview/dist/locale
+import locale from 'iview/dist/locale/zh-CN' // Change locale, check node_modules/iview/dist/locale
 
 Vue.use(iView, {
   locale
