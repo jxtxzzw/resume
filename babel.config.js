98f1d2384059437f109ea0fd84afede6ab2fa473
jxtxzzw <jxtxzzw@126.com>
2020-06-04 18:19:14 +0800

    resume2 refactor init
    Signed-off-by: jxtxzzw <jxtxzzw@126.com>

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,14 @@
 module.exports = {
   presets: [
-    '@vue/cli-plugin-babel/preset',
+    '@vue/app'
   ],
+  env: {
+    test: {
+      plugins: [
+        ['istanbul', {
+          useInlineSourceMaps: false
+        }]
+      ]
+    }
+  }
 }
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -39,12 +39,12 @@
 </template>
 
 <script>
-  export default {
-    name: 'HelloWorld',
-    props: {
-      msg: String,
-    },
-  }
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String,
+  },
+}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
