794d21cba4ac9e0fd0fc53ff19692416d8c3a45a
jxtxzzw <jxtxzzw@126.com>
2020-06-07 15:34:24 +0800

    build
    Signed-off-by: jxtxzzw <jxtxzzw@126.com>

--- src/api/constant.js
+++ src/api/constant.js
@@ -1,5 +1,5 @@
 export const MAIN_URL = 'https://www.jxtxzzw.com';
 
-export const ROOT_URL = `${MAIN_URL}/resume`;
-export const QUERY_URL_INFO = `${ROOT_URL}/api/getInfo.php`;
-export const QUERY_URL_CODE = `${ROOT_URL}/api/getCode.php`;
+export const ROOT_URL = 'https://resume.jxtxzzw.com';
+export const QUERY_URL_INFO = `${ROOT_URL}/resume/api/getInfo.php`;
+export const QUERY_URL_CODE = `${ROOT_URL}/resume/api/getCode.php`;
