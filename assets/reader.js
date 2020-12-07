866a54e661b084d2ac30f2f02cdc66f10d7b7a24
jxtxzzw <jxtxzzw@126.com>
2020-12-07 22:41:29 +0800

    修复：里程碑排序
    Signed-off-by: jxtxzzw <jxtxzzw@126.com>

--- assets/reader.js
+++ assets/reader.js
@@ -9,7 +9,9 @@ export const certificate = sortByDate(
 )
 export const game = sortByDate(require('./data/game.json')[2].data)
 export const fiction = sortByDate(require('./data/fiction.json')[2].data)
-export const milestone = sortByDate(require('./data/milestone.json')[2].data)
+export const milestone = sortByDate(
+  require('./data/milestone.json')[2].data
+).reverse()
 export const media = sortByDate(require('./data/media.json')[2].data)
 export const photo = sortByDate(
   filenameToSrc(require('./data/photo.json')[2].data, setting.src.photo)
