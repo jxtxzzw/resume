f120725c8d37b91fc25c8322d91e973efb4d6e16
jxtxzzw <jxtxzzw@126.com>
2020-12-14 17:35:33 +0800

    优化：支持考试分数基准分非 0
    Signed-off-by: jxtxzzw <jxtxzzw@126.com>

--- pages/exam.vue
+++ pages/exam.vue
@@ -10,11 +10,7 @@
             <i-circle
               v-for="(section, index) in item.sections"
               :key="section"
-              :percent="
-                (parseFloat(item.scores[index]) /
-                  parseFloat(item.totals[index])) *
-                100
-              "
+              :percent="percent(item, index)"
               dashboard
               stroke-color="#2D8CF0"
             >
@@ -26,19 +22,21 @@
                   {{ section }}
                 </p>
               </div>
-              <span>
+              <span v-if="parseFloat(item.zeros[index]) === 0">
                 {{ $t('exam.total') }}
                 <i>
                   {{ item.totals[index] }}
                 </i>
               </span>
+              <span v-else>
+                <i>
+                  {{ item.zeros[index] + '~' + item.totals[index] }}
+                </i>
+              </span>
             </i-circle>
             <i-circle
               key="sum"
-              :percent="
-                (parseFloat(sum(item.scores)) / parseFloat(sum(item.totals))) *
-                100
-              "
+              :percent="percent(item, null)"
               dashboard
               stroke-color="#5cb85c"
             >
@@ -50,10 +48,15 @@
                   {{ $t('exam.sum') }}
                 </p>
               </div>
-              <span>
+              <span v-if="parseFloat(sum(item.zeros)) === 0">
                 {{ $t('exam.total') }}
                 <i>
                   {{ sum(item.totals) }}
+                </i>
+              </span>
+              <span v-else>
+                <i>
+                  {{ sum(item.zeros) + '~' + sum(item.totals) }}
                 </i>
               </span>
             </i-circle>
@@ -74,6 +77,23 @@ export default {
     }
   },
   methods: {
+    percent(item, index) {
+      let p = 0
+      if (index != null) {
+        p =
+          ((parseFloat(item.scores[index]) - parseFloat(item.zeros[index])) /
+            (parseFloat(item.totals[index]) - parseFloat(item.zeros[index]))) *
+          100
+      } else {
+        p =
+          ((parseFloat(this.sum(item.scores)) -
+            parseFloat(this.sum(item.zeros))) /
+            (parseFloat(this.sum(item.totals)) -
+              parseFloat(this.sum(item.zeros)))) *
+          100
+      }
+      return p
+    },
     sum(arr) {
       let sum = 0
       for (const x of arr) {
