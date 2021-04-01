ab8cc8d510cf6c237d140420ca9c4e2b576e6f14
jxtxzzw <jxtxzzw@126.com>
2021-04-01 11:02:26 +0800

    优化：愿望清单样式
    Signed-off-by: jxtxzzw <jxtxzzw@126.com>

--- pages/dream.vue
+++ pages/dream.vue
@@ -1,7 +1,9 @@
 <template>
   <div>
-    <Row v-for="d in dream" :key="d.name" style="margin-bottom: 20px">
-      <Divider> {{ d.name }} </Divider>
+    <Row v-for="d in dream" :key="d.name" style="margin-bottom: 40px">
+      <Col span="4">
+        <Divider> {{ d.name }} </Divider>
+      </Col>
       <a-steps :current="d.current" :status="d.status">
         <a-step>
           <span slot="title"> {{ $t('dream.born') }} </span>
