e2483bf4e74921d30ec1d69f13aa289e8ea68077
jxtxzzw <jxtxzzw@126.com>
2022-10-30 11:38:17 -0700

    使用亮度图（点密度图）表示海量足迹点数据分布情况：单位面积的内点的个数越多，亮度会越亮
    Signed-off-by: jxtxzzw <jxtxzzw@126.com>

--- assets/footprintUtil.js
+++ assets/footprintUtil.js
@@ -1,44 +1,62 @@
 // nuxt 下不能用 import 引入整个依赖，只能用 plugin 的方式引入
 
+// 气泡图地理区域上方会显示不同大小的圆点，圆形面积与其在数据集中的数值会成正比。
+// function setBubbleMap(pointLayer) {
+//   pointLayer
+//     .scale('point_count', {
+//       type: 'quantile', // 根据四分位数将每个聚合点实际包含的点的个数分为五份
+//     })
+//     .size('point_count', [20, 25, 30, 40, 50]) // 根据每个聚合点实际包含的点的个数决定点的大小
+//     .color('point_count', (pc) => {
+//       if (pc === 1) {
+//         return '#4cfd47'
+//       } else if (pc >= 2 && pc <= 5) {
+//         return '#01a31c'
+//       } else if (pc >= 6 && pc <= 10) {
+//         return '#006400'
+//       } else if (pc >= 11 && pc <= 30) {
+//         return '#20a0ff'
+//       } else {
+//         return '#5b8ff9'
+//       }
+//     })
+//     .shape('point_count', ['circle'])
+//     .style({
+//       opacity: 0.75,
+//       strokeWidth: 0.75,
+//     })
+//     .animate(true) // 水波纹效果，可以扩展更多的点动画效果
+// }
+
+// 亮度图又称点密度图，单位面积的内点的个数越多，亮度会越亮，亮度图一般用来表达海量点数据分布情况
+function setDotIntensity(pointLayer) {
+  const DOT_SIZE = 5
+  const INTENSITY_COLOR = '#080298'
+  const STYLE_OPACITY = 1
+
+  pointLayer.size(DOT_SIZE).shape('dot').color(INTENSITY_COLOR).style({
+    opacity: STYLE_OPACITY,
+  })
+}
+
 function getPointLayer(data, that) {
   const { PointLayer } = that.$l7
 
-  return new PointLayer({
+  const pointLayer = new PointLayer({
     name: 'layer',
+  }).source(data, {
+    parser: {
+      type: 'json',
+      x: 'lng',
+      y: 'lat',
+    },
   })
-    .source(data, {
-      cluster: true, // 海量点数据支持聚合显示
-      parser: {
-        type: 'json',
-        x: 'lng',
-        y: 'lat',
-      },
-    })
-    .scale('point_count', {
-      type: 'quantile', // 根据四分位数将每个聚合点实际包含的点的个数分为五份
-    })
-    .size('point_count', [20, 25, 30, 40, 50]) // 根据每个聚合点实际包含的点的个数决定点的大小
-    .color('point_count', (pc) => {
-      if (pc === 1) {
-        return '#4cfd47'
-      } else if (pc >= 2 && pc <= 5) {
-        return '#01a31c'
-      } else if (pc >= 6 && pc <= 10) {
-        return '#006400'
-      } else if (pc >= 11 && pc <= 30) {
-        return '#20a0ff'
-      } else {
-        return '#5b8ff9'
-      }
-    })
-    .shape('point_count', ['circle'])
-    .style({
-      opacity: 0.75,
-      strokeWidth: 0.75,
-    })
+
+  setDotIntensity(pointLayer)
+
+  pointLayer
     .active(true) // hover 高亮显示
     .select(true) // 选中高亮显示
-    .animate(true) // 水波纹效果，可以扩展更多的点动画效果
     .on('click', (e) => {
       if (e.feature.cluster) {
         that.$Message.info(that.$t('footprint.cluster_click'))
@@ -46,6 +64,8 @@ function getPointLayer(data, that) {
         that.$emit('pictures', e.feature.id, e.feature.pictures)
       }
     })
+
+  return pointLayer
 }
 
 export function constructMapAndScene(map, source, that) {
@@ -53,9 +73,12 @@ export function constructMapAndScene(map, source, that) {
   const { GaodeMap, Mapbox } = that.$l7maps
   const { setting } = require('./reader')
 
+  const DA_DI_YUAN_DIAN = [108.5525, 34.3227] // 大地原点
+  const US_CENTRAL_RANDOM = [-97.512224, 38.162039]
+
   // Step 1: 创建 Scene 对象
   const config = {
-    center: [108.5525, 34.3227], // 大地原点
+    center: map === 'china' ? DA_DI_YUAN_DIAN : US_CENTRAL_RANDOM,
     pitch: 0,
     zoom: 4,
     maxZoom: 18,
